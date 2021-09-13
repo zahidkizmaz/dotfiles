037574d11ecd4fa4bb7d27483f2ce07e92b7f414
Zahid Kizmaz <tech@zahid.rocks>
2021-09-13 17:21:49 +0200

    Yank to clipboard

--- nvim/.config/nvim/lua/keybindings.lua
+++ nvim/.config/nvim/lua/keybindings.lua
@@ -1,5 +1,6 @@
 local keymap = vim.api.nvim_set_keymap
 
+keymap('v', '<leader>y',  '"+y', {noremap= true})
 keymap('n', '<c-j>',  '<c-w>j', {noremap= true})
 keymap('n', '<c-k>',  '<c-w>k', {noremap= true})
 keymap('n', '<c-h>',  '<c-w>h', {noremap= true})
