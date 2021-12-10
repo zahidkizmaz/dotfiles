4e78fb2eda6808d6252db142be34588495a220e2
Zahid Kizmaz <tech@zahid.rocks>
2021-12-10 16:23:12 +0100

    Fix pre-commit config

--- nvim/.config/nvim/lua/plugins/onedark-plugin/init.lua
+++ nvim/.config/nvim/lua/plugins/onedark-plugin/init.lua
@@ -2,7 +2,7 @@ vim.cmd([[
 if (has("autocmd") && !has("gui_running"))
 augroup colorset
   autocmd!
-  autocmd ColorScheme * call onedark#set_highlight("Normal", { "fg": { "gui": "#ABB2BF", "cterm": "145", "cterm16" : "7" } })
+  autocmd ColorScheme * call onedark#set_highlight("Normal", {"fg":{"gui": "#ABB2BF", "cterm": "145", "cterm16" : "7"}})
 augroup END
 endif
 
