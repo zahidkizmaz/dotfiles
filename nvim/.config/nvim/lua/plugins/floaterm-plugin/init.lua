a9c41f13d2b26872ada287d45aa8e52ecc0bc1f1
Zahid Kizmaz <tech@zahid.rocks>
2021-12-14 16:37:13 +0100

    Nvim add floaterm-plugin

---
+++ nvim/.config/nvim/lua/plugins/floaterm-plugin/init.lua
@@ -0,0 +1,4 @@
+vim.g.floaterm_width = 0.8
+vim.g.floaterm_height = 0.9
+vim.g.floaterm_autoclose = 1
+vim.g.floaterm_keymap_toggle = "<C-T>"
--- nvim/.config/nvim/lua/plugins/plugins.lua
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -12,6 +12,7 @@ return require("packer").startup(function(use)
 		"nvim-lualine/lualine.nvim",
 		requires = { "kyazdani42/nvim-web-devicons", opt = true },
 	})
+	use("voldikss/vim-floaterm")
 	---------------------
 
 	-----------------
