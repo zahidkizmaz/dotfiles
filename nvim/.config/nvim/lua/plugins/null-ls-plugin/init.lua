cd81b68f92187a6fadfa707a84908324a2c51956
Zahid Kizmaz <tech@zahid.rocks>
2021-12-19 14:41:51 +0100

    Nvim set host python

--- nvim/.config/nvim/lua/plugins/null-ls-plugin/init.lua
+++ nvim/.config/nvim/lua/plugins/null-ls-plugin/init.lua
@@ -3,7 +3,6 @@ local null_ls = require("null-ls")
 null_ls.setup({
 	debug = false,
 	sources = {
-		null_ls.builtins.code_actions.gitsigns,
 		null_ls.builtins.code_actions.proselint,
 		null_ls.builtins.diagnostics.flake8,
 		null_ls.builtins.diagnostics.hadolint,
--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -7,6 +7,8 @@ local global = vim.g
 local buffer_local = vim.bo
 local set_opt = vim.opt
 
+vim.g.python3_host_prog = vim.fn.expand("$HOME/.pyenv/versions/nvim-venv-3.10.0/bin/python")
+
 global.mapleader = ","
 global.did_load_filetypes = 1 -- filetype.nvim setting
 global.onedark_terminal_italics = 2
