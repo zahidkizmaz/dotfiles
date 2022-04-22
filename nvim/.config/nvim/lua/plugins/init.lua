ce6af140c529e0f7d492780dbf205f4ea42dac5b
Zahid Kizmaz <tech@zahid.rocks>
2022-04-22 16:27:48 +0200

    Add btop config

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -135,7 +135,6 @@ return packer.startup(function(use)
 	use({
 		"nvim-telescope/telescope.nvim",
 		requires = { { "nvim-lua/plenary.nvim" } },
-		-- cmd = "Telescope",
 		config = function()
 			require("plugins.telescope-plugin")
 		end,
