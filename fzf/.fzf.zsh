70c2dcd202e16aea3e3d7792f0ff48e9833558bd
Zahid Kizmaz <tech@zahid.rocks>
2022-05-07 13:54:10 +0200

    Config: add .fzf.zsh

---
+++ fzf/.fzf.zsh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+# Fzf completions and key bindings for Linux only.
+[ -f "/usr/share/fzf/completion.zsh" ] && source "/usr/share/fzf/completion.zsh"
+[ -f "/usr/share/fzf/key-bindings.zsh" ] && source "/usr/share/fzf/key-bindings.zsh"
