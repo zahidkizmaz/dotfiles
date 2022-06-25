934807ecc0fd7138a992afc1fc493dccea01a2d7
Zahid Kizmaz <tech@zahid.rocks>
2022-06-25 18:46:24 +0200

    Remove xmodmap file
    * Use setxkbmap to map capslock -> escape

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 # Set key repeat rate
+autorandr --change &
 xset r rate 250 30 &
+# Make Caps Lock an additional Esc and both Shift Keys toggle Caps Lock
+setxkbmap -option caps:escape,shift:both_capslock &
+
 nm-applet &
 blueman-applet &
 picom &
 dunst &
-autorandr --change --force &
 nitrogen --restore &
 xbindkeys
