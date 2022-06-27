8fa8607926a14834886398ca4eff0ab7cc2961c7
Zahid Kizmaz <tech@zahid.rocks>
2022-06-28 00:01:48 +0200

    Autostart improvements

--- bspwm/.config/bspwm/autostart.sh
+++ bspwm/.config/bspwm/autostart.sh
@@ -3,10 +3,14 @@
 # Set key repeat rate
 autorandr --change &
 xset r rate 250 30 &
-nm-applet &
-blueman-applet &
-picom &
-dunst &
+setxkbmap -option caps:escape,shift:both_capslock &
+
+pgrep -x sxhkd >/dev/null || sxhkd &
+pgrep -x picom >/dev/null || picom &
+pgrep -x dunst >/dev/null || dunst &
+pgrep -x polybar >/dev/null || polybar top_bar &
+pgrep -x nm-applet >/dev/null || nm-applet &
+pgrep -x blueman-applet >/dev/null || blueman-applet &
+
 nitrogen --restore &
-polybar top_bar &
 xbindkeys
