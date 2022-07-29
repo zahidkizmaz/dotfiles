9cf9cbfcbd43c90600b04aecb8c8727c92f73d0a
Zahid Kizmaz <tech@zahid.rocks>
2022-07-29 16:51:47 +0200

    Startup changes
    Run: copy server
    
    Don't Run: xbindkeys, nitrogen --restore (duplicate)

--- bspwm/.config/bspwm/autostart.sh
+++ bspwm/.config/bspwm/autostart.sh
@@ -9,8 +9,7 @@ pgrep -x picom >/dev/null || picom &
 pgrep -x dunst >/dev/null || dunst &
 pgrep -x polybar >/dev/null || polybar top_bar &
 pgrep -x nm-applet >/dev/null || nm-applet &
+pgrep -x copyq >/dev/null || copyq --start-server &
 pgrep -x blueman-applet >/dev/null || blueman-applet &
 
-autorandr --change &
-nitrogen --restore &
-exec xbindkeys
+exec autorandr --change
