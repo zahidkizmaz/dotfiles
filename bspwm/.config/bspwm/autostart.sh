33e308c8c06e5fd56d62513dc841d9c6f4c77a79
Zahid Kizmaz <tech@zahid.rocks>
2022-08-06 15:02:44 +0200

    Put .xinitrc back fix autostart

--- bspwm/.config/bspwm/autostart.sh
+++ bspwm/.config/bspwm/autostart.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 
-# Set key repeat rate
-xset r rate 250 30 &
-setxkbmap -option caps:escape,shift:both_capslock &
-
 pgrep -x sxhkd >/dev/null || sxhkd &
 pgrep -x picom >/dev/null || picom &
 pgrep -x dunst >/dev/null || dunst &
-pgrep -x polybar >/dev/null || polybar top_bar &
 pgrep -x nm-applet >/dev/null || nm-applet &
+pgrep -x polybar >/dev/null || polybar top_bar &
 pgrep -x copyq >/dev/null || copyq --start-server &
 pgrep -x blueman-applet >/dev/null || blueman-applet &
 
-nitrogen --restore &
-exec autorandr --change
+exec nitrogen --restore
