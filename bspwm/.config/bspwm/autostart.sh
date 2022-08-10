d2f42f2cfb64bf748d67628cb9eb703d21640104
Zahid Kizmaz <tech@zahid.rocks>
2022-08-10 23:13:14 +0200

    Polybar: run for all monitors

--- bspwm/.config/bspwm/autostart.sh
+++ bspwm/.config/bspwm/autostart.sh
@@ -4,9 +4,13 @@ pgrep -x sxhkd >/dev/null || sxhkd &
 pgrep -x picom >/dev/null || picom &
 pgrep -x dunst >/dev/null || dunst &
 pgrep -x nm-applet >/dev/null || nm-applet &
-pgrep -x polybar >/dev/null || polybar top_bar &
 pgrep -x copyq >/dev/null || copyq --start-server &
 pgrep -x blueman-applet >/dev/null || blueman-applet &
 pgrep -x polkit-gnome-au || /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
+# Run polybar for all monitors
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+  MONITOR=$m polybar --reload top_bar &
+done
 
 exec nitrogen --restore
