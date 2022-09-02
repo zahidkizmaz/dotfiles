989d7eeb91eafb81656995dea1f17c254f99fec7
Zahid Kizmaz <tech@zahid.rocks>
2022-09-02 23:51:02 +0200

    Polybar improvements
    * Use different bars for laptop screen and external screen
    
    * Start polybar with its own script

--- bspwm/.config/bspwm/autostart.sh
+++ bspwm/.config/bspwm/autostart.sh
@@ -6,11 +6,7 @@ pgrep -x dunst >/dev/null || dunst &
 pgrep -x nm-applet >/dev/null || nm-applet &
 pgrep -x copyq >/dev/null || copyq --start-server &
 pgrep -x blueman-applet >/dev/null || blueman-applet &
+pgrep -x polybar >/dev/null || "$HOME/.config/polybar/autostart.sh" &
 pgrep -x polkit-gnome-au || /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-
-# Run polybar for all monitors
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-  MONITOR=$m polybar --reload top_bar &
-done
 
 exec nitrogen --restore
