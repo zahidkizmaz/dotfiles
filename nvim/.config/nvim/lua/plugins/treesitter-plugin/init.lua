435bae397ad48c5a7ab42d7eae81b0e2176f0bf8
Zahid Kizmaz <tech@zahid.rocks>
2023-02-06 16:29:07 +0100

    Nvim: remove rainbow from treesitter config

--- nvim/.config/nvim/lua/plugins/treesitter-plugin/init.lua
+++ nvim/.config/nvim/lua/plugins/treesitter-plugin/init.lua
@@ -5,7 +5,6 @@ require("nvim-treesitter.configs").setup({
     enable = true,
     disable = { "python" }, -- https://github.com/nvim-treesitter/nvim-treesitter/issues/1573
   },
-  rainbow = { enable = true, extended_mode = true },
   textobjects = {
     select = {
       enable = true,
