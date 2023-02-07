c268c831844fbbf2376639dba074d263913def05
Zahid Kizmaz <tech@zahid.rocks>
2023-02-07 14:57:18 +0100

    Nvim: try removing gtisign next hunk keymaps

--- nvim/.config/nvim/lua/plugins/gitsigns-plugin/init.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns-plugin/init.lua
@@ -14,10 +14,6 @@ require("gitsigns").setup({
   keymaps = {
     -- Default keymap options
     noremap = true,
-
-    ["n ]c"] = { expr = true, "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'" },
-    ["n [c"] = { expr = true, "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'" },
-
     ["n <leader>hs"] = "<cmd>Gitsigns stage_hunk<CR>",
     ["v <leader>hs"] = ":Gitsigns stage_hunk<CR>",
     ["n <leader>hu"] = "<cmd>Gitsigns undo_stage_hunk<CR>",
@@ -28,10 +24,6 @@ require("gitsigns").setup({
     ["n <leader>hb"] = '<cmd>lua require"gitsigns".blame_line{full=true}<CR>',
     ["n <leader>hS"] = "<cmd>Gitsigns stage_buffer<CR>",
     ["n <leader>hU"] = "<cmd>Gitsigns reset_buffer_index<CR>",
-
-    -- Text objects
-    ["o ih"] = ":<C-U>Gitsigns select_hunk<CR>",
-    ["x ih"] = ":<C-U>Gitsigns select_hunk<CR>",
   },
   watch_gitdir = {
     interval = 1000,
