12891ca4e873802e1741d36eadc469a2ad3919a0
Zahid Kizmaz <tech@zahid.rocks>
2023-02-20 17:05:08 +0100

    Revert "Nvim: git conflict keymap changes use x"
    This reverts commit a159437c5401dc4bde28f61fc4e87e452fb14127.

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -102,10 +102,10 @@ require("lazy").setup({
       event = "VeryLazy",
       config = function()
         require("git-conflict").setup({ default_mappings = false })
-        vim.keymap.set("n", "xo", "<Plug>(git-conflict-ours)")
-        vim.keymap.set("n", "xt", "<Plug>(git-conflict-theirs)")
-        vim.keymap.set("n", "xb", "<Plug>(git-conflict-both)")
-        vim.keymap.set("n", "x0", "<Plug>(git-conflict-none)")
+        vim.keymap.set("n", "cc", "<Plug>(git-conflict-ours)")
+        vim.keymap.set("n", "cr", "<Plug>(git-conflict-theirs)")
+        vim.keymap.set("n", "cb", "<Plug>(git-conflict-both)")
+        vim.keymap.set("n", "c0", "<Plug>(git-conflict-none)")
         vim.keymap.set("n", "[x", "<Plug>(git-conflict-prev-conflict)")
         vim.keymap.set("n", "]x", "<Plug>(git-conflict-next-conflict)")
       end,
