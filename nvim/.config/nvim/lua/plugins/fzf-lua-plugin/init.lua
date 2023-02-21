ebddb2bb9c08789f829c775670260f60492e18b8
Zahid Kizmaz <tech@zahid.rocks>
2023-02-21 17:49:22 +0100

    Nvim: fzf-lua performance profile and ripgrep improvements

--- nvim/.config/nvim/lua/plugins/fzf-lua-plugin/init.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua-plugin/init.lua
@@ -1,7 +1,25 @@
 local current_dir_seperated_with_dashes = string.gsub(vim.fn.getcwd(), "/", "-"):sub(2)
 local tag_filename = current_dir_seperated_with_dashes .. "-tags"
 local tag_file_location = vim.g.gutentags_cache_dir .. "/" .. tag_filename
+
+local rg_opts = "--iglob !*.git "
+  .. "--iglob !*__pycache__ "
+  .. "--iglob !*.direnv "
+  .. "--iglob !*node_modules "
+  .. "--iglob !*.pytest_cache "
+  .. "--iglob !*.mypy_cache "
+  .. "--iglob !*target "
+  .. "--iglob !tags "
+  .. "--column "
+  .. "--line-number "
+  .. "--no-heading "
+  .. "--color=always "
+  .. "--smart-case "
+  .. "--max-columns=4096 "
+  .. "--hidden "
+
 require("fzf-lua").setup({
+  "max-perf",
   height = 0.95,
   fzf_opts = { ["--layout"] = "default" },
   winopts = {
@@ -22,7 +40,7 @@ require("fzf-lua").setup({
     },
   },
   grep = {
-    rg_opts = "--column --line-number --no-heading --color=always --smart-case --max-columns=4096 --hidden",
+    rg_opts = rg_opts,
     glob_flag = "--hidden --iglob", -- for case sensitive globs use '--glob'
   },
 })
