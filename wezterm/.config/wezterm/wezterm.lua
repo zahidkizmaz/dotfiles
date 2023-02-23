eae8335cc9888ee058e7fb5d0079c5a51c84fa91
Zahid Kizmaz <tech@zahid.rocks>
2023-02-23 23:59:31 +0100

    Wezterm: create cofig

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require("wezterm")
+return {
+  window_padding = {
+    top = 0,
+    left = 0,
+    right = 0,
+    bottom = 0,
+  },
+  enable_tab_bar = false,
+  window_decorations = "NONE",
+  color_scheme = "Catppuccin Mocha",
+  font = wezterm.font("JetBrainsMono Nerd Font"),
+}
