8df89e5cf14712071bfbe2da20e8c3b64f3e6680
Zahid Kizmaz <tech@zahid.rocks>
2023-02-24 10:23:52 +0100

    Wezterm: add window padding and window decorations should be RESIZE

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,13 +1,14 @@
 local wezterm = require("wezterm")
 return {
   window_padding = {
-    top = 0,
-    left = 0,
-    right = 0,
+    top = 4,
+    left = 2,
+    right = 2,
     bottom = 0,
   },
   enable_tab_bar = false,
-  window_decorations = "NONE",
+  window_decorations = "RESIZE",
   color_scheme = "Catppuccin Mocha",
   font = wezterm.font("JetBrainsMono Nerd Font"),
+  font_size = 14,
 }
