f9e2e4f831f4fc431785b6466a5e7a2102edb150
Zahid Kizmaz <tech@zahid.rocks>
2023-07-08 19:59:25 +0200

    nvim: simplify lua_ls setup

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -53,17 +53,8 @@ lspconfig.lua_ls.setup({
   settings = {
     Lua = {
       completion = { callSnippet = "Replace" }, -- comes from folke/neodev
-      runtime = { version = "LuaJIT" },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { "vim" },
-      },
       format = { enable = false },
-      workspace = {
-        checkThirdParty = false,
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
+      workspace = { checkThirdParty = false },
       -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = { enable = false },
     },
