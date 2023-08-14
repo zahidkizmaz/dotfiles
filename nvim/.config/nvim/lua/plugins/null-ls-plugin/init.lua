d7133c374879aa8d7943a9d9675b754f36e70470
Zahid Kizmaz <tech@zahid.rocks>
2023-08-14 15:13:29 +0200

    nvim: remove most of the null-ls config

--- nvim/.config/nvim/lua/plugins/null-ls-plugin/init.lua
+++ nvim/.config/nvim/lua/plugins/null-ls-plugin/init.lua
@@ -3,47 +3,11 @@ local null_ls = require("null-ls")
 null_ls.setup({
   debug = false,
   sources = {
-    null_ls.builtins.code_actions.gitsigns,
-    null_ls.builtins.diagnostics.ruff,
-    null_ls.builtins.diagnostics.shellcheck,
-    null_ls.builtins.diagnostics.yamllint,
-    null_ls.builtins.diagnostics.curlylint,
-    null_ls.builtins.diagnostics.ansiblelint,
-    null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.diagnostics.pmd.with({
       filetypes = { "apexcode" },
       args = { "pmd", "--dir", "$ROOT/force-app", "--format", "json" },
       extra_args = { "-R", "pmd-apex-ruleset.xml", "--cache", vim.fn.expand("$HOME/.cache/pmd") },
     }),
-    null_ls.builtins.formatting.black.with({ extra_args = { "--fast" } }),
-    null_ls.builtins.formatting.isort,
-    null_ls.builtins.formatting.djhtml.with({
-      extra_args = function(params)
-        local file_name = vim.api.nvim_buf_get_name(params.bufnr)
-        local tabwidth = vim.api.nvim_buf_get_option(params.bufnr, "shiftwidth")
-        return {
-          "--tabwidth",
-          tabwidth,
-          file_name,
-        }
-      end,
-    }),
-    null_ls.builtins.formatting.prettier.with({
-      filetypes = { "apexcode" },
-      extra_args = { "--plugin=prettier-plugin-apex" },
-    }), -- Just for apexcode
-    null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.formatting.rustfmt,
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.trim_newlines,
-    null_ls.builtins.formatting.shfmt.with({
-      filetypes = { "sh", "bash", "zsh" },
-      extra_args = { "-i=2", "-ci", "-bn" },
-    }),
-    null_ls.builtins.formatting.sqlfluff.with({
-      extra_args = { "--dialect", "postgres" },
-    }),
-    null_ls.builtins.formatting.rustfmt,
   },
   debounce = vim.opt.updatetime:get(),
   update_on_insert = false,
