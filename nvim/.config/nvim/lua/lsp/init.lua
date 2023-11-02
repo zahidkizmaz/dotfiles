ccbc447187ec39668f5f84c897c0ab414db74e02
Zahid Kizmaz <tech@zahid.rocks>
2023-11-02 20:25:52 +0100

    nvim: update apex-lsp.jar location

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -93,7 +93,7 @@ lspconfig.rust_analyzer.setup({
   on_attach = on_attach_without_formatting,
 })
 lspconfig.apex_ls.setup({
-  apex_jar_path = vim.fn.expand("$HOME/apex-jorje-lsp.jar"),
+  apex_jar_path = vim.fn.expand("$HOME/apex-lsp/apex-jorje-lsp.jar"),
   apex_enable_semantic_errors = true, -- Whether to allow Apex Language Server to surface semantic errors
   apex_enable_completion_statistics = false, -- Whether to allow Apex Language Server to collect telemetry on code completion usage
 })
