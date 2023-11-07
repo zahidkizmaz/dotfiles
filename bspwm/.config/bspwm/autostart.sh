a99c2962942adfaf54a7dd9b42517fdaee0ab02e
Zahid Kizmaz <tech@zahid.rocks>
2023-11-07 08:15:07 +0100

    bspwm: remove albert

--- bspwm/.config/bspwm/autostart.sh
+++ bspwm/.config/bspwm/autostart.sh
@@ -3,7 +3,6 @@
 pgrep -x sxhkd >/dev/null || sxhkd &
 pgrep -x picom >/dev/null || picom &
 pgrep -x dunst >/dev/null || dunst &
-pgrep -x albert >/dev/null || albert --quiet &
 pgrep -x nm-applet >/dev/null || nm-applet &
 pgrep -x copyq >/dev/null || copyq --start-server &
 pgrep -x blueman-applet >/dev/null || blueman-applet &
