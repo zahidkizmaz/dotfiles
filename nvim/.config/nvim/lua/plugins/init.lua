e3a6d7ec4b00517086878cdb3cb18aeebe83347b
Zahid Kizmaz <tech@zahid.rocks>
2023-11-17 18:39:57 +0100

    nvim: add rainbow-delimiters.nvim

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -333,6 +333,29 @@ require("lazy").setup({
       dependencies = {
         { "nvim-treesitter/nvim-treesitter-refactor" },
         { "nvim-treesitter/nvim-treesitter-context", config = true },
+        {
+          "https://gitlab.com/HiPhish/rainbow-delimiters.nvim",
+          config = function()
+            local rainbow_delimiters = require("rainbow-delimiters")
+
+            vim.g.rainbow_delimiters = {
+              strategy = {
+                [""] = rainbow_delimiters.strategy["global"],
+                vim = rainbow_delimiters.strategy["local"],
+              },
+              query = { [""] = "rainbow-delimiters" },
+              highlight = {
+                "RainbowDelimiterRed",
+                "RainbowDelimiterYellow",
+                "RainbowDelimiterBlue",
+                "RainbowDelimiterOrange",
+                "RainbowDelimiterGreen",
+                "RainbowDelimiterViolet",
+                "RainbowDelimiterCyan",
+              },
+            }
+          end,
+        },
       },
     },
     {
