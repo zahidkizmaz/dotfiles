13595bdebb2bcccc134b21b683537a5e55d4a092
Zahid Kizmaz <tech@zahid.rocks>
2024-01-09 13:08:27 +0100

    nvim: apex_ls shoud attach to apex files

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -103,6 +103,7 @@ lspconfig.apex_ls.setup({
   apex_jar_path = vim.fn.expand("$HOME/apex-lsp/apex-jorje-lsp.jar"),
   apex_enable_semantic_errors = true, -- Whether to allow Apex Language Server to surface semantic errors
   apex_enable_completion_statistics = false, -- Whether to allow Apex Language Server to collect telemetry on code completion usage
+  filetypes = { "apexcode", "apex" },
 })
 lspconfig.efm.setup({
   on_attach = on_attach_with_formatting,
