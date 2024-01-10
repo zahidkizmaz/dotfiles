054a5250d4ba99833379f34c148aa52d2054875e
Zahid Kizmaz <tech@zahid.rocks>
2024-01-10 17:46:38 +0100

    shfmt: fix shfmt in precommit and nvim

--- bspwm/.config/bspwm/multimonitor_workspaces.sh
+++ bspwm/.config/bspwm/multimonitor_workspaces.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 monitor_count=$(bspc query -M | wc -l)
-if [[ "$monitor_count" == 1 ]]; then
+if [[ $monitor_count == 1 ]]; then
   bspc monitor -d I II III IV V VI VII VIII IX X
-elif [[ "$monitor_count" == 2 ]]; then
+elif [[ $monitor_count == 2 ]]; then
   bspc monitor "$(bspc query -M | awk NR==1)" -d I II III IV V
   bspc monitor "$(bspc query -M | awk NR==2)" -d VI VII VIII IX X
-elif [[ "$monitor_count" == 3 ]]; then
+elif [[ $monitor_count == 3 ]]; then
   bspc monitor "$(bspc query -M | awk NR==1)" -d I II III IV
   bspc monitor "$(bspc query -M | awk NR==2)" -d V VI VII
   bspc monitor "$(bspc query -M | awk NR==3)"-d VIII IX X
-elif [[ "$monitor_count" == 4 ]]; then
+elif [[ $monitor_count == 4 ]]; then
   bspc monitor "$(bspc query -M | awk NR==1)" -d I II III
   bspc monitor "$(bspc query -M | awk NR==2)" -d IV V VI
   bspc monitor "$(bspc query -M | awk NR==3)" -d VII VIII
   bspc monitor "$(bspc query -M | awk NR==4)" -d IX X
-elif [[ "$monitor_count" == 5 ]]; then
+elif [[ $monitor_count == 5 ]]; then
   bspc monitor "$(bspc query -M | awk NR==1)" -d I II
   bspc monitor "$(bspc query -M | awk NR==2)" -d III IV
   bspc monitor "$(bspc query -M | awk NR==3)" -d V VI
