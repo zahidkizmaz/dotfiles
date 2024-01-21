5ca0b3426eb6e78e909db0c0a803338e10ca0b8b
Zahid Kizmaz <tech@zahid.rocks>
2024-01-21 15:43:32 +0100

    nvim: improve lsp setup performance

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -1,108 +1,160 @@
-local LSP_SERVERS = {
-  "ansiblels",
-  "apex_ls",
-  "bashls",
-  "cssls",
-  "dockerls",
-  "efm",
-  "jsonls",
-  "lemminx",
-  "lua_ls",
-  "pylsp",
-  "rust_analyzer",
-  "tailwindcss",
-  "tsserver",
-  "vimls",
-  "yamlls",
-}
-local CUSTOM_CONFIGURED_SERVERS = { "lua_ls", "pylsp", "yamlls", "apex_ls", "efm", "jsonls", "rust_analyzer" }
-
-require("mason").setup()
-require("mason-lspconfig").setup({
-  ensure_installed = LSP_SERVERS,
-  automatic_installation = true,
-})
-
-require("neodev").setup({})
 local lspconfig = require("lspconfig")
 local lsp_handlers = require("lsp.handlers")
-local capabilities = lsp_handlers.capabilities
 local schemastore = require("schemastore")
 
-require("lspconfig.ui.windows").default_options.border = "rounded"
-
-for _, server in ipairs(LSP_SERVERS) do
-  if not vim.tbl_contains(CUSTOM_CONFIGURED_SERVERS, server) then
-    lspconfig[server].setup({ capabilities = capabilities, handlers = lsp_handlers.handlers })
-  end
-end
-
-lspconfig.rust_analyzer.setup({
-  capabilities = capabilities,
-  handlers = lsp_handlers.handlers,
-  settings = {
-    ["rust-analyzer"] = {
-      checkOnSave = { command = "clippy" },
+local LSP_SERVERS = {
+  ansiblels = { pattern = { "*.yaml", ".yml" } },
+  apex_ls = {
+    pattern = { "*.cls", "*.trigger" },
+    setup_config = {
+      apex_jar_path = vim.fn.expand("$HOME/apex-lsp/apex-jorje-lsp.jar"),
+      apex_enable_semantic_errors = true, -- Whether to allow Apex Language Server to surface semantic errors
+      apex_enable_completion_statistics = false, -- Whether to allow Apex Language Server to collect telemetry on code completion usage
+      filetypes = { "apexcode", "apex" },
     },
   },
-})
-lspconfig.jsonls.setup({
-  capabilities = capabilities,
-  handlers = lsp_handlers.handlers,
-  settings = {
-    json = {
-      schemas = schemastore.json.schemas(),
-      validate = { enable = true },
+  bashls = {
+    pattern = { "*.sh", "*.zsh", "*rc", "*zsh*" },
+    setup_config = {
+      capabilities = lsp_handlers.capabilities,
+      handlers = lsp_handlers.handlers,
+      filetypes = { "sh", "zsh" },
     },
   },
-})
-lspconfig.lua_ls.setup({
-  capabilities = capabilities,
-  handlers = lsp_handlers.handlers,
-  settings = {
-    Lua = {
-      completion = { callSnippet = "Replace" }, -- comes from folke/neodev
-      format = { enable = false },
-      workspace = { checkThirdParty = false },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = { enable = false },
-    },
+  cssls = {
+    pattern = { "*.css", "*.scss" },
   },
-})
-lspconfig.pylsp.setup({
-  capabilities = capabilities,
-  handlers = lsp_handlers.handlers,
-  settings = {
-    format = { enable = false },
-    pylsp = {
-      plugins = {
-        yapf = { enabled = false },
-        mccabe = { enabled = false },
-        pyflakes = { enabled = false },
-        autopep8 = { enabled = false },
-        pydocstyle = { enabled = false },
-        pycodestyle = { enabled = false },
-        rope_autoimport = { enabled = false }, -- Currently doesn't work!
-        jedi_signature_help = { enabled = true },
-        pylsp_mypy = {
-          dmypy = true,
-          enabled = true,
-          live_mode = false,
-          report_progress = true,
+  dockerls = {
+    pattern = { "*Dockerfile*" },
+  },
+  efm = {
+    pattern = { "*" },
+    setup_config = { handlers = lsp_handlers.handlers, init_options = { documentFormatting = true } },
+  },
+  jsonls = {
+    pattern = { "*.json" },
+    setup_config = {
+      capabilities = lsp_handlers.capabilities,
+      handlers = lsp_handlers.handlers,
+      settings = {
+        json = {
+          schemas = schemastore.json.schemas(),
+          validate = { enable = true },
         },
       },
     },
   },
-})
-lspconfig.apex_ls.setup({
-  apex_jar_path = vim.fn.expand("$HOME/apex-lsp/apex-jorje-lsp.jar"),
-  apex_enable_semantic_errors = true, -- Whether to allow Apex Language Server to surface semantic errors
-  apex_enable_completion_statistics = false, -- Whether to allow Apex Language Server to collect telemetry on code completion usage
-  filetypes = { "apexcode", "apex" },
-})
-lspconfig.efm.setup({
-  handlers = lsp_handlers.handlers,
-  init_options = { documentFormatting = true },
-})
+  lemminx = {
+    pattern = { "*.xml" },
+  },
+  lua_ls = {
+    pattern = { "*.lua" },
+    setup_config = {
+      capabilities = lsp_handlers.capabilities,
+      handlers = lsp_handlers.handlers,
+      settings = {
+        Lua = {
+          completion = { callSnippet = "Replace" }, -- comes from folke/neodev
+          format = { enable = false },
+          workspace = { checkThirdParty = false },
+          -- Do not send telemetry data containing a randomized but unique identifier
+          telemetry = { enable = false },
+        },
+      },
+    },
+  },
+  pylsp = {
+    pattern = { "*.py" },
+    setup_config = {
+      capabilities = lsp_handlers.capabilities,
+      handlers = lsp_handlers.handlers,
+      settings = {
+        format = { enable = false },
+        pylsp = {
+          plugins = {
+            yapf = { enabled = false },
+            mccabe = { enabled = false },
+            pyflakes = { enabled = false },
+            autopep8 = { enabled = false },
+            pydocstyle = { enabled = false },
+            pycodestyle = { enabled = false },
+            rope_autoimport = { enabled = false }, -- Currently doesn't work!
+            jedi_signature_help = { enabled = true },
+            pylsp_mypy = {
+              dmypy = true,
+              enabled = true,
+              live_mode = false,
+              report_progress = true,
+            },
+          },
+        },
+      },
+    },
+  },
+  rust_analyzer = {
+    pattern = { "*.rs" },
+    setup_config = {
+      capabilities = lsp_handlers.capabilities,
+      handlers = lsp_handlers.handlers,
+      settings = {
+        ["rust-analyzer"] = {
+          checkOnSave = { command = "clippy" },
+        },
+      },
+    },
+  },
+  tailwindcss = {
+    pattern = { "*.css", "*.scss" },
+  },
+  tsserver = {
+    pattern = { "*.js", "*.ts", "*.jsx", "*.tsx" },
+  },
+  vimls = {
+    pattern = { "*.lua" },
+  },
+  yamlls = {
+    pattern = { "*.yaml", "*.yml" },
+    setup_config = {
+      capabilities = lsp_handlers.capabilities,
+      handlers = lsp_handlers.handlers,
+      settings = {
+        yaml = {
+          keyOrdering = false,
+          schemaStore = {
+            -- disable built-in schemaStore support if you want to use
+            -- schemastore plugin and its advanced options like `ignore`.
+            enable = false,
+            -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+            url = "",
+          },
+          schemas = schemastore.yaml.schemas(),
+        },
+      },
+    },
+  },
+}
+
+for name, config in pairs(LSP_SERVERS) do
+  local default_setup_config = { capabilities = lsp_handlers.capabilities, handlers = lsp_handlers.handlers }
+  local setup_config = config.setup_config or default_setup_config
+
+  local group = vim.api.nvim_create_augroup("setup_lsp_config_" .. name, { clear = true })
+  vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
+    group = group,
+    pattern = config.pattern,
+    callback = function()
+      if not vim.tbl_isempty(vim.lsp.get_clients({ name = name })) then
+        return
+      end
+
+      if name == "lua_ls" then
+        require("neodev").setup({})
+      end
+
+      lspconfig[name].setup(setup_config)
+    end,
+  })
+end
 
 lsp_handlers.setup()
+require("lspconfig.ui.windows").default_options.border = "rounded"
