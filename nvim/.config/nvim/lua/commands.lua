ea9d68d5c3eb37a533213143a5a381271bdb041d
Zahid Kizmaz <tech@zahid.rocks>
2024-01-21 23:46:50 +0100

    nvim: remove depr nvim_get_option

--- nvim/.config/nvim/lua/commands.lua
+++ nvim/.config/nvim/lua/commands.lua
@@ -46,8 +46,8 @@ end, {
 
 function Gitui()
   local cmd = "gitui"
-  local width = vim.api.nvim_get_option("columns")
-  local height = vim.api.nvim_get_option("lines")
+  local width = vim.api.nvim_win_get_width(0)
+  local height = vim.api.nvim_win_get_height(0)
   local window_options = {
     style = "minimal",
     relative = "editor",
