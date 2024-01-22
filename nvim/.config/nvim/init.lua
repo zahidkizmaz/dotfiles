4b1816174b6497e25e289e234786d3a0cee267a8
Zahid Kizmaz <tech@zahid.rocks>
2024-01-22 15:02:48 +0100

    nvim: keybindings to keymaps

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,4 @@
-function Prequire(module_str)
+local function prequire(module_str)
   local status_ok, module = pcall(require, module_str)
   if status_ok then
     return module
@@ -7,7 +7,7 @@ function Prequire(module_str)
   end
 end
 
-Prequire("settings")
-Prequire("plugins")
-Prequire("commands")
-Prequire("keybindings")
+prequire("settings")
+prequire("plugins")
+prequire("commands")
+prequire("keymaps")
