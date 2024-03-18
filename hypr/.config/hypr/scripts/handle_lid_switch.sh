f079b2d981ddf81d53bc5b7f7376a6f27feeb50f
Zahid Kizmaz <tech@zahid.rocks>
2024-03-18 18:15:22 +0100

    hypr: fix monitor setup on lid switch

--- hypr/.config/hypr/scripts/handle_lid_switch.sh
+++ hypr/.config/hypr/scripts/handle_lid_switch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if grep open /proc/acpi/button/lid/LID0/state; then
-  hyprctl keyword monitor "eDP-1,preferred,auto,1"
+  hyprctl keyword monitor "eDP-1,preferred,auto,1.175"
 else
   if [[ "$(hyprctl monitors | grep -c 'Monitor')" -gt 1 ]]; then
     hyprctl keyword monitor "eDP-1, disable"
