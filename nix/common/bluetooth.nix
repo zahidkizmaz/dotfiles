858bfffd23487940c1f0f010543f84f9112eabb7
Zahid Kizmaz <tech@zahid.rocks>
2024-04-12 21:38:38 +0200

    update packages

--- nix/common/bluetooth.nix
+++ nix/common/bluetooth.nix
@@ -1,8 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   # Enable Bluetooth
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = false;
   services.blueman.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    blueman
+  ];
 }
