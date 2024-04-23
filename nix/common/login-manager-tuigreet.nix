db20e4eeee3bb9dd4c0bf3a0703925a7ddede553
Zahid Kizmaz <tech@zahid.rocks>
2024-04-23 10:06:06 +0200

    tuigreet remember user

--- nix/common/login-manager-tuigreet.nix
+++ nix/common/login-manager-tuigreet.nix
@@ -5,7 +5,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
         user = "greeter";
       };
     };
