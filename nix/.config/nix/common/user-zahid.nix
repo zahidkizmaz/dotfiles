2c4ee4c3432c11700f307461e52ad60487d2f4dc
Zahid Kizmaz <tech@zahid.rocks>
2024-05-18 22:25:07 +0200

    add nixpkgs-fmt

--- nix/.config/nix/common/user-zahid.nix
+++ nix/.config/nix/common/user-zahid.nix
@@ -13,44 +13,44 @@
     extraGroups = [ "networkmanager" "input" "wheel" "video" "audio" "storage" "cups" ];
     shell = pkgs.zsh;
     packages = with pkgs; [
-        atuin
-        bat
-        copyq
-        curl
-        fd
-        feh
-        firefox
-        flatpak
-        fzf
-        git
-        just
-        jq
-        kitty
-        man
-        neofetch
-        neovim
-        powertop
-        python3
-        ripgrep
-        rustup
-        shfmt
-        starship
-        stow
-        stylua
-        tmux
-        tree
-        universal-ctags
-        unzip
-        viu
-        wget
-        zip
-        zoxide
-      ];
+      atuin
+      bat
+      copyq
+      curl
+      fd
+      feh
+      firefox
+      flatpak
+      fzf
+      git
+      just
+      jq
+      kitty
+      man
+      neofetch
+      neovim
+      powertop
+      python3
+      ripgrep
+      rustup
+      shfmt
+      starship
+      stow
+      stylua
+      tmux
+      tree
+      universal-ctags
+      unzip
+      viu
+      wget
+      zip
+      zoxide
+    ];
   };
   programs.zsh.enable = true;
 
   fonts.packages = with pkgs; [
-      nerdfonts
-      noto-fonts-color-emoji
+    nerdfonts
+    noto-fonts-color-emoji
   ];
 }
