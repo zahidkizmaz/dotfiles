4176164113ec01c67c4031d95a17fa68bd0a45fd
Zahid Kizmaz <tech@zahid.rocks>
2024-05-20 21:45:39 +0200

    add rpi4b

---
+++ nix/.config/nix/hosts/pi4b/disk-config.nix
@@ -0,0 +1,33 @@
+{
+  disko.devices = {
+    disk = {
+      vdb = {
+        device = "/dev/sda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              end = "500M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              name = "root";
+              end = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
