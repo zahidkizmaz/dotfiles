baacd320359f4a5675fee14d5f724d166a1d01a2
Zahid Kizmaz <tech@zahid.rocks>
2024-05-25 15:34:21 +0200

    some improvements pi4b

--- nix/.config/nix/hosts/pi4b/configuration.nix
+++ nix/.config/nix/hosts/pi4b/configuration.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, user, ... }:
 {
   imports = [
     ./hardware-configuration.nix
@@ -14,15 +14,27 @@
     wireless.enable = false;
 
     firewall.enable = true;
-    firewall.allowedTCPPorts = [ 22 80 443 ];
+    firewall.allowedTCPPorts = [ 22 443 ];
   };
 
-  users.users.pi = {
+  users.users.${user} = {
     isNormalUser = true;
     shell = pkgs.zsh;
     extraGroups = [ "networkmanager" "input" "wheel" "video" "audio" "storage" ];
   };
-  programs.zsh.enable = true;
+  programs = {
+    zsh = {
+      enable = true;
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
+    };
+    starship.enable = true;
+  };
+
+  fonts.packages = with pkgs; [
+    fira-code-nerdfont
+    noto-fonts-color-emoji
+  ];
 
   i18n.defaultLocale = "en_US.UTF-8";
   console = { keyMap = "us"; };
@@ -33,6 +45,10 @@
     magic-wormhole
     neovim
   ];
+
+  # Limit update size/frequency of rebuilds
+  # Also preserve space on SD card
+  documentation.nixos.enable = false;
 
   system.stateVersion = "24.05";
 }
