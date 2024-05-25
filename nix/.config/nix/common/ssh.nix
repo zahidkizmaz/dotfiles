0b4415de07aefcf2a22aa04be76e82247d624035
Zahid Kizmaz <tech@zahid.rocks>
2024-05-25 15:34:21 +0200

    some improvements pi4b

--- nix/.config/nix/common/ssh.nix
+++ nix/.config/nix/common/ssh.nix
@@ -1,7 +1,12 @@
-{ ... }:
+{ user, ... }:
 {
-  services.openssh.enable = true;
-  users.users.root.openssh.authorizedKeys.keys = [
+  services.openssh = {
+    enable = true;
+    # Start a systemd service for each incoming SSH connection
+    startWhenNeeded = true;
+  };
+
+  users.users.${user}.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ4fh0Rr/ASYtfbx+rY9JUm25tnhIxobntSVbrSfZQ7o tech@zahid.rocks"
   ];
 }
