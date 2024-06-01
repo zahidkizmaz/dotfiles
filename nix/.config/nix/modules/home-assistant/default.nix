b3cebf59703707951ab964383eb5fdd95835c8ae
Zahid Kizmaz <tech@zahid.rocks>
2024-06-01 09:15:52 +0200

    home assistant remove custom config

--- nix/.config/nix/modules/home-assistant/default.nix
+++ nix/.config/nix/modules/home-assistant/default.nix
@@ -11,15 +11,7 @@
     package = (pkgs.home-assistant.override { extraPackages = ps: [ ps.psycopg2 ]; });
   };
   services.home-assistant.config =
-    let
-      hiddenEntities = [
-        "sensor.last_boot"
-        "sensor.date"
-      ];
-    in
     {
-      icloud = { };
-      frontend = { };
       http = {
         use_x_forwarded_for = true;
         trusted_proxies = [
@@ -27,27 +19,5 @@
           "::1"
         ];
       };
-      history.exclude = {
-        entities = hiddenEntities;
-        domains = [
-          "automation"
-          "updater"
-        ];
-      };
-      "map" = { };
-      shopping_list = { };
-      backup = { };
-      logbook.exclude.entities = hiddenEntities;
-      logger.default = "info";
-      sun = { };
-      config = { };
-      mobile_app = { };
-
-      cloud = { };
-      network = { };
-      zeroconf = { };
-      system_health = { };
-      default_config = { };
-      system_log = { };
     };
 }
