5dbaa9c83f9d4ae97949c44fe6bb456203759ddc
Zahid Kizmaz <tech@zahid.rocks>
2024-06-03 07:01:01 +0200

    enable postgres

--- nix/.config/nix/modules/home-assistant/postgres.nix
+++ nix/.config/nix/modules/home-assistant/postgres.nix
@@ -1,8 +1,11 @@
+{ pkgs, ... }:
 {
   services.home-assistant.config = {
     recorder.db_url = "postgresql://@/hass";
   };
   services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_16;
     ensureDatabases = [ "hass" ];
     ensureUsers = [
       {
