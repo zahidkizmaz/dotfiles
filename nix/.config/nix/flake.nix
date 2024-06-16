a10bc21e1175f3b3b4ecb9f235fba1f22e01be59
Zahid Kizmaz <tech@zahid.rocks>
2024-06-16 22:53:51 +0200

    remove firefox-addons cause it broke the update

--- nix/.config/nix/flake.nix
+++ nix/.config/nix/flake.nix
@@ -9,10 +9,6 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
-    firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
