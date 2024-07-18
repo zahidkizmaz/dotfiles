1a510a29ec8f588a0d66677083e3beada0c12cab
Zahid Kizmaz <tech@zahid.rocks>
2024-07-18 21:45:20 +0200

    fixes

--- nixos/modules/user-zahid.nix
+++ nixos/modules/user-zahid.nix
@@ -14,10 +14,28 @@
     extraGroups = [ "networkmanager" "input" "wheel" "video" "audio" "storage" "cups" ];
     shell = pkgs.zsh;
     packages = with pkgs; [
+      atuin
+      bat
+      copyq
+      delta
+      easyeffects
+      fd
       feh
+      fzf
+      gitMinimal
+      just
+      jq
       kitty
+      man
+      neovim
+      ripgrep
+      starship
+      stow
+      tmux
+      tree
       unzip
       zip
+      zoxide
     ];
   };
   programs.zsh.enable = true;
@@ -31,7 +49,13 @@
   system.userActivationScripts = {
     desktop-env.text =
       ''
+        ln -sfn /home/${user}/dotfiles/atuin/.config/atuin /home/${user}/.config/atuin
+        ln -sfn /home/${user}/dotfiles/bat/.config/bat /home/${user}/.config/bat
+        ln -sfn /home/${user}/dotfiles/git/.config/git /home/${user}/.config/git
         ln -sfn /home/${user}/dotfiles/kitty/.config/kitty /home/${user}/.config/kitty
+        ln -sfn /home/${user}/dotfiles/starship/.config/starship.toml /home/${user}/.config/starship.toml
+        ln -sfn /home/${user}/dotfiles/tmux/.config/tmux /home/${user}/.config/tmux
+        ln -sfn /home/${user}/dotfiles/zsh/.config/zsh /home/${user}/.config/zsh
       '';
   };
 }
