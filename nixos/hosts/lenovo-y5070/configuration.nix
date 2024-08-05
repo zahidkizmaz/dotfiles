3a0e7211ab694e449496604656a19c434db1e223
Zahid Kizmaz <tech@zahid.rocks>
2024-08-05 12:12:10 +0200

    lenovo enable graphics and firefox hardware acceleration

--- nixos/hosts/lenovo-y5070/configuration.nix
+++ nixos/hosts/lenovo-y5070/configuration.nix
@@ -5,6 +5,15 @@
     ./disko.nix
   ];
 
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      libvdpau-va-gl
+    ];
+  };
+
   networking = {
     hostName = "y5070";
   };
