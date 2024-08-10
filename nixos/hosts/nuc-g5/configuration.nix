eadb0a5c3de684a61fa44cb8e44b94624ea58c0c
Zahid Kizmaz <tech@zahid.rocks>
2024-08-10 12:02:50 +0200

    g5 add boot.kernel and initrd settings

--- nixos/hosts/nuc-g5/configuration.nix
+++ nixos/hosts/nuc-g5/configuration.nix
@@ -6,6 +6,11 @@
   ];
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
