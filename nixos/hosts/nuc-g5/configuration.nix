d6575dfa8056c93d67f0a73c629cee2d46a3a11a
Zahid Kizmaz <tech@zahid.rocks>
2024-08-10 08:34:31 +0200

    g5 add installer

---
+++ nixos/hosts/nuc-g5/configuration.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, lib, user, modulesPath, ... }:
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    ./disko.nix
+  ];
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  swapDevices = [{
+    device = "/var/lib/swapfile";
+    size = 32 * 1024; # 32GB
+  }];
+
+
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      libvdpau-va-gl
+    ];
+  };
+
+  console = { keyMap = "us"; };
+  i18n.defaultLocale = "en_US.UTF-8";
+  time.timeZone = "Europe/Berlin";
+  networking = {
+    hostName = "nuc-g5";
+    useDHCP = lib.mkDefault true;
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+  users.users.${user} = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    extraGroups = [ "input" "wheel" "video" "audio" "storage" ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    gitMinimal
+    neovim
+  ];
+
+  system.stateVersion = "24.05";
+}
