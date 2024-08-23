8a7e0a8f7446285f0983cec39e01c35b76e403c8
Zahid Kizmaz <tech@zahid.rocks>
2024-08-23 14:39:05 +0200

    create virtualisation.vmVariant configuration

---
+++ nixos/modules/vm-variant.nix
@@ -0,0 +1,10 @@
+{...}:
+{
+  virtualisation.vmVariant = {
+    # following configuration is added only when building VM with build-vm
+    virtualisation = {
+      memorySize =  8192; # in MB
+      cores = 4;
+    };
+  };
+}
