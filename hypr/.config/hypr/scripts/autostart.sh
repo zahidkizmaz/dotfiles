5150bd93fe7f37eec9aedc7c4797f59d0f524a6d
Zahid Kizmaz <tech@zahid.rocks>
2024-09-16 22:53:55 +0200

    remove -x from pgrep it doesn't work on nix

--- hypr/.config/hypr/scripts/autostart.sh
+++ hypr/.config/hypr/scripts/autostart.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-pgrep -x blueman-applet >/dev/null || blueman-applet &
-pgrep -x copyq >/dev/null || copyq --start-server &
-pgrep -x dunst >/dev/null || dunst &
-pgrep -x hypridle >/dev/null || hypridle &
-pgrep -x nm-applet >/dev/null || nm-applet &
-pgrep -x pcmanfm >/dev/null || pcmanfm --start-server &
-pgrep -x waybar >/dev/null || waybar &
+pgrep blueman-applet >/dev/null || blueman-applet &
+pgrep copyq >/dev/null || copyq --start-server &
+pgrep dunst >/dev/null || dunst &
+pgrep hypridle >/dev/null || hypridle &
+pgrep nm-applet >/dev/null || nm-applet &
+pgrep pcmanfm >/dev/null || pcmanfm --start-server &
+pgrep waybar >/dev/null || waybar &
