0901f27fca8c54fad54a841baffcbed5a503a063
Zahid Kizmaz <tech@zahid.rocks>
2024-09-20 16:57:29 +0200

    add alias for llm serve

--- nixos/modules/ollama.nix
+++ nixos/modules/ollama.nix
@@ -3,8 +3,13 @@ let
   pkgs-unstable = import inputs.nixpkgs-unstable { inherit system; };
 in
 {
-  environment.systemPackages = with pkgs-unstable; [
-    ollama
-    nextjs-ollama-llm-ui
-  ];
+  environment = {
+    systemPackages = with pkgs-unstable; [
+      ollama
+      nextjs-ollama-llm-ui
+    ];
+    interactiveShellInit = ''
+      alias llms='ollama serve && nextjs-ollama-llm-ui'
+    '';
+  };
 }
