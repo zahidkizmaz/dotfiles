6ac4bb14bcf9c07d5f9c830053170091ba3fd59e
Zahid Kizmaz <tech@zahid.rocks>
2024-09-20 08:39:47 +0200

    add ollama nix

---
+++ nixos/modules/ollama.nix
@@ -0,0 +1,10 @@
+{ inputs, system, ... }:
+let
+  pkgs-unstable = import inputs.nixpkgs-unstable { inherit system; };
+in
+{
+  environment.systemPackages = with pkgs-unstable; [
+    ollama
+    open-webui
+  ];
+}
