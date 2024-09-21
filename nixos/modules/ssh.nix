9f3c6b532b5a27c1e7fc89617801773bb628c2ff
Zahid Kizmaz <tech@zahid.rocks>
2024-09-21 09:40:51 +0200

    ssh start agent on startup

--- nixos/modules/ssh.nix
+++ nixos/modules/ssh.nix
@@ -5,8 +5,10 @@
     # Start a systemd service for each incoming SSH connection
     startWhenNeeded = true;
   };
+  programs.ssh.startAgent = true;
 
   users.users.${user}.openssh.authorizedKeys.keys = [
+    # lab
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ4fh0Rr/ASYtfbx+rY9JUm25tnhIxobntSVbrSfZQ7o tech@zahid.rocks"
   ];
 }
