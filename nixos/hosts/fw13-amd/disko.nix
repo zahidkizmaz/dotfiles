cd0f6d9a3556f46ea6f6caa31356714d9895ef88
Zahid Kizmaz <tech@zahid.rocks>
2024-09-23 07:22:50 +0200

    fw13 disko more descriptive device

--- nixos/hosts/fw13-amd/disko.nix
+++ nixos/hosts/fw13-amd/disko.nix
@@ -3,7 +3,7 @@
     disk = {
       main = {
         type = "disk";
-        device = "/dev/disk/by-diskseq/1";
+        device = "/dev/nvme0n1";
         content = {
           type = "gpt";
           partitions = {
