cfc861ab30f3b4e0abbbffd70cc2ac689c796fe4
Zahid Kizmaz <tech@zahid.rocks>
2024-09-29 14:01:53 +0200

    use progress bar in volume notifications

--- hypr/.config/hypr/scripts/change-volume.sh
+++ hypr/.config/hypr/scripts/change-volume.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Arbitrary but unique message id
-msgId="9999"
+msgId="987123"
 
 action="${1}"
 action_sign="${action: -1}"
@@ -15,52 +15,34 @@ change_volume() {
 
 # Set volume and icon based on the action
 case $action_sign in
-  "+")
-    icon="󰝝"
-    change_volume "$action"
-    ;;
-  "-")
-    icon="󰝞"
-    change_volume "$action"
-    ;;
-  "e")
-    # ends with 'e' from 'toggle'
-    icon="󰝟"
-    wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-    ;;
-  *)
-    echo "Usage: $0 [5%+|5%-|toggle]"
-    exit 1
-    ;;
+"+")
+  icon="󰝝"
+  change_volume "$action"
+  ;;
+"-")
+  icon="󰝞"
+  change_volume "$action"
+  ;;
+"e")
+  # ends with 'e' from 'toggle'
+  icon="󰝟"
+  wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+  ;;
+*)
+  echo "ERROR: Example Usage: $0 [5%+|5%-|toggle]"
+  exit 1
+  ;;
 esac
 
 # Query volume level
-volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print int($2 * 100)}')
-mute=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep MUTED)
+volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
+volume_level=$(awk '{print int($2 * 100)}' <<<"$volume")
+muted=$(grep MUTED <<<"$volume")
 
-# Define the width of the volume bar (adjust as needed)
-bar_width=28
-
-generate_bar() {
-  local width=$1
-  local percent=$2
-  local filled_width=$((width * percent / 100))
-  local empty_width=$((width - filled_width))
-
-  if [[ $filled_width != 0 ]]; then
-    printf '█%.0s' $(seq 1 $filled_width)
-  fi
-  if [ $empty_width != 0 ]; then
-    printf '░%.0s' $(seq 1 $empty_width)
-  fi
-}
-
-if [[ $volume == 0 || -n $mute ]]; then
+if [[ $volume_level == 0 || -n $muted ]]; then
   # Show the sound muted notification
-  dunstify -a "changeVolume" -u low -r "$msgId" "$icon  Muted" "$(generate_bar $bar_width 0)"
+  notify-send -a "changeVolume" -u critical -r "$msgId" "$icon  Muted" -h "int:value:${volume_level}"
 else
   # Show the volume notification with a visual bar
-  bar=$(generate_bar $bar_width "$volume")
-  dunstify -a "changeVolume" -u low -r "$msgId" \
-    "$icon  ${volume}%" "$bar"
+  notify-send -a "changeVolume" -u low -r "$msgId" "$icon  Volume " -h "int:value:${volume_level}"
 fi
