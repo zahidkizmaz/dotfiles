ed1ecdc8e435bbc8a07ba233a6b18b19175c9dac
Zahid Kizmaz <tech@zahid.rocks>
2024-10-24 23:33:56 +0200

    nvim: tabnine ignore files that has special characters in the name

--- nvim/.config/nvim/lua/plugins/cmp-plugin/tabnine.lua
+++ nvim/.config/nvim/lua/plugins/cmp-plugin/tabnine.lua
@@ -4,6 +4,16 @@ require("cmp_tabnine.config"):setup({
   run_on_every_keystroke = true,
 })
 
+local function has_special_chars(str)
+  local special_chars = { "{", "}" }
+  for _, special_char in ipairs(special_chars) do
+    if string.find(str, special_char) then
+      return true
+    end
+  end
+  return false
+end
+
 local function cmp_prefetch_git_files()
   local root_git_dir = vim.fn.systemlist({ "git", "rev-parse", "--show-toplevel" }) or {}
   if not vim.tbl_isempty(root_git_dir) then
@@ -11,9 +21,12 @@ local function cmp_prefetch_git_files()
 
     local git_files = vim.fn.systemlist({ "git", "ls-files" }) or {}
     for _, file in ipairs(git_files) do
-      local file_path = root_git_dir_path .. "/" .. file
-      require("cmp_tabnine"):prefetch(vim.fn.expand(file_path))
+      if not has_special_chars(file) then
+        local file_path = root_git_dir_path .. "/" .. file
+        require("cmp_tabnine"):prefetch(vim.fn.expand(file_path))
+      end
     end
   end
 end
+
 vim.schedule(cmp_prefetch_git_files)
