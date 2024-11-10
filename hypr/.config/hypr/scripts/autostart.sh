a56301b7918e504d84a8b9109ede45c164bab83a
Zahid Kizmaz <tech@zahid.rocks>
2024-11-10 21:19:26 +0100

    hyprland improve autostart

--- hypr/.config/hypr/scripts/autostart.sh
+++ hypr/.config/hypr/scripts/autostart.sh
@@ -1,9 +1,43 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-pgrep blueman-applet >/dev/null || blueman-applet &
-pgrep copyq >/dev/null || copyq --start-server &
-pgrep dunst >/dev/null || dunst &
-pgrep hypridle >/dev/null || hypridle &
-pgrep nm-applet >/dev/null || nm-applet &
-pgrep pcmanfm >/dev/null || pcmanfm --start-server &
-pgrep waybar >/dev/null || waybar &
+start_program() {
+  program_name="$1"
+  args="$2"
+
+  if ! pgrep "$program_name" >/dev/null; then
+    if [ -z "$args" ]; then
+      nohup "$program_name" >/dev/null 2>&1 &
+    else
+      nohup "$program_name" "$args" >/dev/null 2>&1 &
+    fi
+    echo "Started $program_name $args with PID $!"
+  fi
+}
+
+restart_program() {
+  program_name="$1"
+  args="$2"
+
+  pid=$(pgrep "$program_name")
+  if [ -n "$pid" ]; then
+    kill "$pid"
+    echo "Killed $program_name with PID $pid"
+    sleep 0.5
+  fi
+
+  if [ -z "$args" ]; then
+    nohup "$program_name" >/dev/null 2>&1 &
+  else
+    nohup "$program_name" "$args" >/dev/null 2>&1 &
+  fi
+  echo "Started $program_name $args with PID $!"
+}
+
+start_program "blueman-applet"
+start_program "copyq" "--start-server"
+start_program "hypridle"
+start_program "nm-applet"
+start_program "pcmanfm" "--daemon-mode"
+start_program "dunst"
+
+restart_program "waybar"
