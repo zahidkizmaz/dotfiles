a778aa87c1ddd5903af6ca8e76eb6d5b1558b0e9
Zahid Kizmaz <tech@zahid.rocks>
2024-11-14 22:21:09 +0100

    flake update

--- nixos/modules/bluetooth.nix
+++ nixos/modules/bluetooth.nix
@@ -1,12 +1,8 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   # Enable Bluetooth
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = false;
   services.blueman.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    blueman
-  ];
 }
