36552938cdea6daca6f5fa7ea5389d08adb906f4
Zahid Kizmaz <tech@zahid.rocks>
2024-11-18 19:47:34 +0100

    reduce boot loader timeout

--- nixos/modules/bootloader-systemd.nix
+++ nixos/modules/bootloader-systemd.nix
@@ -3,7 +3,7 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.timeout = 5;
+  boot.loader.timeout = 3;
   boot.initrd.enable = true;
   boot.initrd.systemd.enable = true;
 }
