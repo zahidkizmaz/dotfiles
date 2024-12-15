57eaf3b5df16a7986813e14498aab3bd9d5e863a
Zahid Kizmaz <tech@zahid.rocks>
2024-12-15 14:02:29 +0100

    low battery notifications and libcamera

--- nixos/modules/libcamera.nix
+++ nixos/modules/libcamera.nix
@@ -8,7 +8,4 @@ in
   environment.systemPackages = with pkgs-unstable; [
     libcamera-qcam
   ];
-
-  systemd.user.services.wireplumber.environment.LIBCAMERA_IPA_PROXY_PATH = "${pkgs.libcamera-qcam}/libexec/libcamera";
-
 }
