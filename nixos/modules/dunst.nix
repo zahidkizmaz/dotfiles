9e535a59e9386b04eb1b4c4ce0e3a2834b1401c8
Zahid Kizmaz <tech@zahid.rocks>
2024-12-15 14:02:29 +0100

    low battery notifications and libcamera

--- nixos/modules/dunst.nix
+++ nixos/modules/dunst.nix
@@ -6,6 +6,9 @@
     libnotify
   ];
 
+  services.udev.extraRules = ''
+    SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_NAME}=="BAT1", ENV{POWER_SUPPLY_STATUS}=="Discharging", ENV{POWER_SUPPLY_CAPACITY_LEVEL}=="Critical", RUN+="${pkgs.libnotify}/bin/notify-send -i battery-empty -u critical 'Low Battery!' "
+  '';
 
   system.userActivationScripts = {
     dunst-env.text /*bash*/ =
