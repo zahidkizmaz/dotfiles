c6d2734b37b5322bb860e7c24530e71c66815440
Zahid Kizmaz <tech@zahid.rocks>
2024-12-16 19:21:12 +0100

    try to fix low low-battery-notification udev rule

--- nixos/modules/dunst.nix
+++ nixos/modules/dunst.nix
@@ -1,13 +1,26 @@
 { pkgs, user, ... }:
-{
+let
+  lowBatteryNotification = pkgs.writeShellApplication {
+    name = "low-battery-notification";
+    runtimeInputs = [ pkgs.libnotify ];
+    text /*bash*/ = ''
+      #! ${pkgs.runtimeShell}
 
+      set -x
+      echo "Hello zahid"
+      notify-send -i battery-empty -u critical 'Low Battery!'
+    '';
+  };
+in
+{
   environment.systemPackages = with pkgs; [
     dunst
     libnotify
+    lowBatteryNotification
   ];
 
   services.udev.extraRules = ''
-    SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_NAME}=="BAT1", ENV{POWER_SUPPLY_STATUS}=="Discharging", ENV{POWER_SUPPLY_CAPACITY_LEVEL}=="Critical", RUN+="${pkgs.libnotify}/bin/notify-send -i battery-empty -u critical 'Low Battery!' "
+    SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_NAME}=="BAT1", ENV{POWER_SUPPLY_STATUS}=="Discharging", ATTR{capacity_level}=="Critical|Low", RUN+="${lowBatteryNotification}/bin/low-battery-notification"
   '';
 
   system.userActivationScripts = {
