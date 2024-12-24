f70abf0bdc570fe46117575329904776e52de3a1
Zahid Kizmaz <tech@zahid.rocks>
2024-12-24 11:22:14 +0100

    nvim: fix treesitter setup

--- nvim/.config/nvim/lua/plugins/code.lua
+++ nvim/.config/nvim/lua/plugins/code.lua
@@ -26,16 +26,18 @@ return {
     build = ":TSUpdate",
     event = { "BufRead", "BufNewFile" },
     cmd = { "TSUpdate", "TSUpdateSync" }, -- Needed for headless runs such as: nvim --headless -c "TSUpdateSync" +qa
-    opts = {
-      auto_install = true,
-      sync_install = false,
-      ignore_install = {},
-      ensure_installed = {},
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { "python" }, -- https://github.com/nvim-treesitter/nvim-treesitter/issues/1573
-      },
-    },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        auto_install = true,
+        sync_install = false,
+        ignore_install = {},
+        ensure_installed = {},
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+      })
+    end,
     dependencies = {
       {
         "RRethy/vim-illuminate",
