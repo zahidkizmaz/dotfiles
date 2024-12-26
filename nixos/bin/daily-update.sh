18f24edaafa17edbaab2fc13ba0e35c8fd25385f
Zahid <mzkizmaz@gmail.com>
2024-12-26 21:55:39 +0000

    Add new lines in daily-update.sh

--- nixos/bin/daily-update.sh
+++ nixos/bin/daily-update.sh
@@ -1,24 +1,24 @@
 if command -v "zeesh-update" >/dev/null 2>&1; then
-  echo "Updating zeesh plugins"
+  echo "\nUpdating zeesh plugins"
   zeesh-update
 fi
 
 if command -v "bat" >/dev/null 2>&1; then
-  echo "Building bat cache"
+  echo "\nBuilding bat cache"
   bat cache --build
 fi
 
 if command -v "tldr" >/dev/null 2>&1; then
-  echo "Updating tldr"
+  echo "\nUpdating tldr"
   tldr --update
 fi
 
 if command -v "brew" >/dev/null 2>&1; then
-  echo "Updating brew packages"
+  echo "\nUpdating brew packages"
   brew update && brew upgrade && brew upgrade --cask && brew cleanup && brew autoremove
 fi
 
 if command -v "nvim" >/dev/null 2>&1; then
-  echo "Updating nvim packages"
+  echo "\nUpdating nvim packages"
   nvim --headless -c "Lazy! sync" -c "TSUpdateSync all" +qa
 fi
