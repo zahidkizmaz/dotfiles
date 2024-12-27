7e2096fa1e3ba4cb499475ee18d07475686540f7
Zahid Kizmaz <tech@zahid.rocks>
2024-12-27 01:58:24 +0100

    sort modules

--- nixos/hosts/mondo/default.nix
+++ nixos/hosts/mondo/default.nix
@@ -6,12 +6,12 @@ inputs.nix-darwin.lib.darwinSystem {
   modules = [
     ./configuration.nix
     ./homebrew.nix
+    ../../modules/ai.nix
     ../../modules/documentation.nix
     ../../modules/dotfiles.nix
     ../../modules/kitty.nix
     ../../modules/nix-settings.nix
     ../../modules/nvim.nix
-    ../../modules/ai.nix
     ../../modules/zsh.nix
   ];
   specialArgs = { inherit inputs system; user = "zahidkizmaz"; };
