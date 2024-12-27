71dd7e9f643e56aeab233f52bdd593cf4a9ffece
Zahid Kizmaz <tech@zahid.rocks>
2024-12-27 01:32:34 +0100

    fix shellcheck error

--- nixos/bin/daily-update.sh
+++ nixos/bin/daily-update.sh
@@ -1,24 +1,24 @@
 if command -v "zeesh-update" >/dev/null 2>&1; then
-  echo "\nUpdating zeesh plugins"
+  printf "\nUpdating zeesh plugins\n"
   zeesh-update
 fi
 
 if command -v "bat" >/dev/null 2>&1; then
-  echo "\nBuilding bat cache"
+  printf "\nBuilding bat cache\n"
   bat cache --build
 fi
 
 if command -v "tldr" >/dev/null 2>&1; then
-  echo "\nUpdating tldr"
+  printf "\nUpdating tldr\n"
   tldr --update
 fi
 
 if command -v "brew" >/dev/null 2>&1; then
-  echo "\nUpdating brew packages"
+  printf "\nUpdating brew packages\n"
   brew update && brew upgrade && brew upgrade --cask && brew cleanup && brew autoremove
 fi
 
 if command -v "nvim" >/dev/null 2>&1; then
-  echo "\nUpdating nvim packages"
+  printf "\nUpdating nvim packages\n"
   nvim --headless -c "Lazy! sync" -c "TSUpdateSync all" +qa
 fi
