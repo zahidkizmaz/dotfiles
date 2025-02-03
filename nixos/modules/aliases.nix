fc96e0bca01c2109f6778e3a74ff52133edcd1a6
Zahid Kizmaz <tech@zahid.rocks>
2025-02-03 13:45:23 +0100

    nixos: update aliases.nix

--- nixos/modules/aliases.nix
+++ nixos/modules/aliases.nix
@@ -1,13 +1,7 @@
-{ pkgs, ... }:
-let
-  fzf = "${pkgs.fzf}/bin/fzf";
-  sesh = "${pkgs.sesh}/bin/sesh";
-in
+{ ... }:
 {
   environment.shellAliases = {
-    ff = "${pkgs.fastfetch}/bin/fastfetch";
-    btop = "${pkgs.btop}/bin/btop";
-    ls = "${pkgs.eza}/bin/eza";
-    sl = "${sesh} connect $(${sesh} list | ${fzf})";
+    btop = "nix run nixpkgs#btop";
+    ff = "nix run nixpkgs#fastfetch";
   };
 }
