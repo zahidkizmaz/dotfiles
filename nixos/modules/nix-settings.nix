242693116a4493a1324af4aa6fce1d0ae98974a7
Zahid Kizmaz <tech@zahid.rocks>
2025-02-03 13:49:47 +0100

    nixos: add nixPath for nixd

--- nixos/modules/nix-settings.nix
+++ nixos/modules/nix-settings.nix
@@ -1,16 +1,19 @@
-{ ... }:
+{ inputs, ... }:
 {
-  nix.settings = {
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-    substituters = [
-      "https://nix-community.cachix.org"
-      "https://cache.nixos.org/"
-    ];
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
+  nix = {
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+    nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
   };
 }
