c858f2e7ab558819da503aef3538206f76e189a3
Zahid Kizmaz <tech@zahid.rocks>
2025-03-27 08:22:14 +0100

    nixos: pipewire configure to use less power

--- nixos/modules/sound-pipewire.nix
+++ nixos/modules/sound-pipewire.nix
@@ -1,15 +1,23 @@
 { pkgs, ... }:
 {
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    pulse.enable = true;
     jack.enable = true;
     wireplumber.enable = true;
+
+    # For less power consumption
+    extraConfig.pipewire."92-low-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 512;
+        "default.clock.min-quantum" = 128;
+        "default.clock.max-quantum" = 2048;
+      };
+    };
   };
 
   environment.systemPackages = with pkgs; [
