7fca75464da2fa19a87baf90921184ca49c35b3a
Zahid Kizmaz <tech@zahid.rocks>
2025-03-27 08:36:56 +0100

    nixos: pipewire consumption tweak

--- nixos/modules/sound-pipewire.nix
+++ nixos/modules/sound-pipewire.nix
@@ -13,9 +13,9 @@
     extraConfig.pipewire."92-low-latency" = {
       "context.properties" = {
         "default.clock.rate" = 48000;
-        "default.clock.quantum" = 512;
+        "default.clock.quantum" = 256;
         "default.clock.min-quantum" = 128;
-        "default.clock.max-quantum" = 2048;
+        "default.clock.max-quantum" = 1024;
       };
     };
   };
