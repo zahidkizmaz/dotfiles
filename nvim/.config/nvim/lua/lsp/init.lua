7d21113f591934df23049b60b198f13cd0a35a2c
Zahid Kizmaz <tech@zahid.rocks>
2025-03-31 23:06:24 +0200

    neovim: lua_ls improvements

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -57,7 +57,16 @@ M.SERVER_CONFIGURATIONS = {
         settings = {
           Lua = {
             format = { enable = false },
-            workspace = { checkThirdParty = false },
+            workspace = {
+              checkThirdParty = false,
+              -- Make the server aware of Neovim runtime files
+              library = vim.api.nvim_get_runtime_file("", true),
+            },
+
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = { "vim" },
+            },
             -- Do not send telemetry data containing a randomized but unique identifier
             telemetry = { enable = false },
           },
