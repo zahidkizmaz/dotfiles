f110a5d8d3123edb970d073ba571d7b4b6510293
Zahid Kizmaz <tech@zahid.rocks>
2025-04-09 19:57:43 +0200

    nixos: pi4b add fan control service

---
+++ nixos/hosts/pi4b/fan-control.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    haskellPackages.gpio
+    libraspberrypi
+  ];
+
+  # service to control the fan
+  systemd.services.fan-control = {
+    description = "Control the fan depending on the temperature";
+    script = ''
+      /run/current-system/sw/bin/gpio init 18 out
+      temperature=$(/run/current-system/sw/bin/vcgencmd measure_temp | grep -oE '[0-9]+([.][0-9]+)?')
+      threshold=65
+      if /run/current-system/sw/bin/awk -v temp="$temperature" -v threshold="$threshold" 'BEGIN { exit !(temp > threshold) }'; then
+        /run/current-system/sw/bin/gpio write 18 hi
+      else
+        /run/current-system/sw/bin/gpio write 18 lo
+      fi
+      /run/current-system/sw/bin/gpio close 18 out
+    '';
+  };
+
+  systemd.timers.fan-control-timer = {
+    description = "Run control fan script regularly";
+    timerConfig = {
+      OnCalendar = "*-*-* *:0/1:00"; # Run every 10 minutes
+      Persistent = true;
+      Unit = "fan-control.service";
+    };
+    wantedBy = [ "timers.target" ];
+  };
+}
