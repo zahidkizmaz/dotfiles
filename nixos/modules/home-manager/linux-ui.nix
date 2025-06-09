a50b83f89aec5dd84dd0c6808fa656f292c2410e
Zahid Kizmaz <tech@zahid.rocks>
2025-06-09 22:31:37 +0200

    nixos: update linux-ui to set preferred theme dark

--- nixos/modules/home-manager/linux-ui.nix
+++ nixos/modules/home-manager/linux-ui.nix
@@ -36,15 +36,20 @@
     };
 
     gtk3.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
+      gtk-application-prefer-dark-theme = 1;
     };
 
     gtk4.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
+      gtk-application-prefer-dark-theme = 1;
+    };
+  };
+
+  dconf = {
+    enable = true;
+    settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
     };
   };
 }
--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -6,6 +6,7 @@ return {
   },
   {
     "zbirenbaum/copilot.lua",
+    enabled = false,
     event = "InsertEnter",
     cmd = "Copilot",
     opts = {
