514069acb5b3f8435c8852acd104f0b2f248d171
Zahid Kizmaz <tech@zahid.rocks>
2025-07-27 21:53:55 +0200

    hypr: move hyprland to unstable packages and enable drag_3fg

--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -1,12 +1,16 @@
-{ pkgs, ... }:
+{ inputs, system, ... }:
+let
+  pkgs-unstable = import inputs.nixpkgs-unstable { inherit system; };
+in
 {
   programs = {
     hyprland.enable = true;
     hyprland.withUWSM = true;
+    hyprland.package = pkgs-unstable.hyprland;
     hyprlock.enable = true;
   };
 
-  environment.systemPackages = with pkgs; [
+  environment.systemPackages = with pkgs-unstable; [
     hypridle
     hyprlock
     hyprsunset
