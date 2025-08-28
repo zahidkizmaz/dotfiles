25fb2ef8c38d67d7e3fc36c31bfd6a82cc5a0491
Zahid Kizmaz <tech@zahid.rocks>
2025-08-28 22:17:34 +0200

    hypr: automate hyprsunset

--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -19,4 +19,45 @@ in
     hyprpolkitagent
     hyprland-qt-support
   ];
+
+  systemd.user.services."hyprsunset-automate" = {
+    serviceConfig = {
+      Type = "simple";
+    };
+    enable = true;
+    startAt = "*:0/1";
+    path = with pkgs-unstable; [
+      bash
+      hyprsunset
+    ];
+    script = ''
+      START_HOUR=21
+      STOP_HOUR=9
+
+      declare -i current_hour
+      current_hour=$(date +%k)
+
+      if ((START_HOUR > STOP_HOUR)); then
+        if ((current_hour >= START_HOUR || current_hour < STOP_HOUR)); then
+          if ! pgrep -x hyprsunset >/dev/null; then
+            echo "INFO: Starting hyprsunset current_hour:$current_hour"
+            "${pkgs-unstable.hyprsunset}/bin/hyprsunset" -t 3500
+          fi
+        else
+          echo "INFO: Stopping hyprsunset current_hour:$current_hour"
+          pkill -x hyprsunset
+        fi
+      else
+        if ((current_hour >= START_HOUR && current_hour < STOP_HOUR)); then
+          if ! pgrep -x hyprsunset >/dev/null; then
+            echo "INFO: Starting hyprsunset current_hour:$current_hour"
+            "${pkgs-unstable.hyprsunset}/bin/hyprsunset" -t 3500
+          fi
+        else
+          echo "INFO: Stopping hyprsunset current_hour:$current_hour"
+          pkill -x hyprsunset
+        fi
+      fi
+    '';
+  };
 }
