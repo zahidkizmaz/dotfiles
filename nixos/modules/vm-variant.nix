a43b6b0e2326d677cdb2c66fc4f73c8888226dbf
Zahid Kizmaz <tech@zahid.rocks>
2025-09-07 14:54:01 +0200

    nixos: vmVariant increase diskSize

--- nixos/modules/vm-variant.nix
+++ nixos/modules/vm-variant.nix
@@ -3,6 +3,7 @@
   virtualisation.vmVariant = {
     # following configuration is added only when building VM with build-vm
     virtualisation = {
+      diskSize = 10000; # disk size in MiB (about 10GB)
       memorySize = 8192; # in MB
       cores = 4;
     };
