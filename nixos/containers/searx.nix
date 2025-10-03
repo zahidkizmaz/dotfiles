2ddda94e5fdec193c0b361ccf3fe613119b0e959
Zahid Kizmaz <tech@zahid.rocks>
2025-10-03 09:18:32 +0200

    nixos: searx setup container network

--- nixos/containers/searx.nix
+++ nixos/containers/searx.nix
@@ -73,6 +73,16 @@ in
           };
         };
 
+        networking = {
+          firewall = {
+            enable = true;
+          };
+          # Use systemd-resolved inside the container
+          # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
+          useHostResolvConf = lib.mkForce false;
+        };
+        services.resolved.enable = true;
+
         system.stateVersion = stateVersion;
       };
   };
