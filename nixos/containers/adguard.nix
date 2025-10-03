cde19a950d7c3649240c5896c41d61c11f46b9d7
Zahid Kizmaz <tech@zahid.rocks>
2025-10-03 22:42:45 +0200

    nixos: adguardhome declare settings

--- nixos/containers/adguard.nix
+++ nixos/containers/adguard.nix
@@ -9,6 +9,11 @@
 }:
 let
   containerName = "ad";
+  filters = [
+    "https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt"
+    "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/pro.txt"
+    "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+  ];
 in
 {
   containers.${containerName} = {
@@ -48,9 +53,28 @@ in
 
         services.adguardhome = {
           enable = true;
-          port = port;
-          host = "0.0.0.0";
+          mutableSettings = false;
           openFirewall = true;
+          settings = {
+            inherit filters;
+            port = port;
+            http.address = "0.0.0.0";
+            dns = {
+              ratelimit = 0;
+              bind_hosts = [ "0.0.0.0" ];
+              upstream_dns = [
+                "9.9.9.9"
+                "1.1.1.1"
+                "1.0.0.1"
+              ];
+              bootstrap_dns = [
+                "9.9.9.10"
+                "149.112.112.10"
+                "2620:fe::10"
+                "2620:fe::fe:10"
+              ];
+            };
+          };
         };
 
         system.stateVersion = stateVersion;
