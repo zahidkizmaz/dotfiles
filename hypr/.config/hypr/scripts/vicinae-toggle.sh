0cf319256843ce27ffe7e02feb972c0837c4e2c9
Zahid Kizmaz <tech@zahid.rocks>
2025-11-29 23:04:10 +0100

    fix viciane

--- hypr/.config/hypr/scripts/vicinae-toggle.sh
+++ hypr/.config/hypr/scripts/vicinae-toggle.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 if ! pgrep "vicinae" >/dev/null; then
-  vicinae server &
+  USE_LAYER_SHELL=0 nohup vicinae server >/tmp/vicinae.log 2>&1 &
+  sleep 0.3
 fi
 
 vicinae toggle
