6e264c5ea3a714082b1b752763d4640b3c346e85
Zahid Kizmaz <tech@zahid.rocks>
2025-12-01 09:54:26 +0100

    nixos(darwin): update flake to 25.11

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     };
 
     nix-darwin = {
-      url = "github:LnL7/nix-darwin/nix-darwin-25.05";
+      url = "github:LnL7/nix-darwin/nix-darwin-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
