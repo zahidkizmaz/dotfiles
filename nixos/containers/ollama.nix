cd885ae27470cd5616ec85d8e445cffb227e04ab
Zahid Kizmaz <tech@zahid.rocks>
2026-06-14 23:46:46 +0200

    feat(nixos/containers): add ollama container module

---
+++ nixos/containers/ollama.nix
@@ -0,0 +1,62 @@
+{
+  stateVersion,
+  localAddress,
+  hostAddress,
+  inputs,
+  user,
+  models,
+  ...
+}:
+let
+  containerName = "ollama";
+  port = 11434;
+in
+{
+  containers.${containerName} = {
+    autoStart = true;
+    privateNetwork = true;
+    privateUsers = "identity";
+    enableTun = true;
+    ephemeral = false;
+    hostAddress = hostAddress;
+    localAddress = localAddress;
+    bindMounts = {
+      "/etc/ssh/lab" = {
+        hostPath = "/home/${user}/.ssh/lab";
+        isReadOnly = true;
+      };
+    };
+    config =
+      {
+        config,
+        pkgs,
+        lib,
+        ...
+      }:
+      {
+        imports = [
+          ./container-common.nix
+          (import ./container-tailscale.nix {
+            inherit
+              config
+              inputs
+              lib
+              pkgs
+              port
+              ;
+          })
+        ];
+
+        services.ollama = {
+          enable = true;
+          host = "0.0.0.0";
+          port = port;
+          openFirewall = true;
+          package = pkgs.ollama-cpu;
+          loadModels = models;
+        };
+
+        system.stateVersion = stateVersion;
+      };
+  };
+}
