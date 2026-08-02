5d4b47e45bb1dafec217d020cdfee40dfb006f93
Zahid Kizmaz <tech@zahid.rocks>
2026-08-02 19:47:16 +0200

    add unofficial mcp

--- nixos/modules/home-assistant/default.nix
+++ nixos/modules/home-assistant/default.nix
@@ -50,6 +50,9 @@ in
       mini-graph-card
       universal-remote-card
     ];
+    customComponents = with pkgs.home-assistant-custom-components; [
+      ha_mcp_tools
+    ];
     config = {
       http = {
         use_x_forwarded_for = true;
