139f85ccd3483ae411e1459a64f51cc900dae6d9
Zahid Kizmaz <tech@zahid.rocks>
2026-08-02 11:07:30 +0200

    fix: restore cache.nixos.org key to trusted-public-keys

--- flake.nix
+++ flake.nix
@@ -9,6 +9,7 @@
       "https://nixos-raspberrypi.cachix.org"
     ];
     trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
       "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
